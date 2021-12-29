6d496c1f1946b0fe273d290d394cc7ac38896d57
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-29 18:00:27 +0530

    ADDED: Gambler loose or win

--- src/com/gambler/Gambler.java
+++ src/com/gambler/Gambler.java
@@ -6,5 +6,12 @@ public class Gambler {
     public static void main(String[] args) {
         System.out.println("Welcome to gambler simulation");
         System.out.println("initial amount is "+INITIAL_STAKE +" Daily bet amount "+BET_AMOUNT);
+        double randomNumber = Math.random();
+        System.out.println("Random number is :" +randomNumber);
+        if (randomNumber>0.5){
+            System.out.println("gambler won the bet");
+        }else {
+            System.out.println("gambler lose the bet");
+        }
     }
 }
