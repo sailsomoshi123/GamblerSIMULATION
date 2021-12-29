8f7d50704d3e375a94f152ea28dce866217324a8
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-29 17:45:57 +0530

    ADDED: Initial condition added

--- src/com/gambler/Gambler.java
+++ src/com/gambler/Gambler.java
@@ -1,7 +1,10 @@
 package com.gambler;
 
 public class Gambler {
+    public static int INITIAL_STAKE = 100;
+    public static int BET_AMOUNT = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to gambler simulation");
+        System.out.println("initial amount is "+INITIAL_STAKE +" Daily bet amount "+BET_AMOUNT);
     }
 }
