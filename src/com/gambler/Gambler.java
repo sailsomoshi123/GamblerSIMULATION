52b1e5829e1956a9fa516af8632c1847deb7fa1c
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-03 14:58:28 +0530

    ADDED :Lukiest and unlukiest day

--- src/com/gambler/Gambler.java
+++ src/com/gambler/Gambler.java
@@ -1,20 +1,24 @@
 package com.gambler;
+import java.util.Arrays;
 
 public class Gambler {
     public static int INITIAL_STAKE = 100;
     public static int BET_AMOUNT = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to gambler simulation");
-        System.out.println("initial amount is "+INITIAL_STAKE +" Daily bet amount "+BET_AMOUNT);
-        int allAmount=100;
-        int minStack=(allAmount*50)/100;
-        int maxStack=allAmount+minStack;
-        int winCount=0;
-        int lostCount=0;
-        int winAmount=0;
-        int lostAmount=0;
-        for (int i=1; i<=31;i++) {
-            int totalAmount = INITIAL_STAKE;
+        System.out.println("initial amount is " + INITIAL_STAKE + " Daily bet amount " + BET_AMOUNT);
+        int allAmount = 100;
+        int minStack = (allAmount * 50) / 100;
+        int maxStack = allAmount + minStack;
+        int winCount = 0;
+        int lostCount = 0;
+        int winAmount = 0;
+        int lostAmount = 0;
+        int winDay = 0;
+        int lostDay = 0;
+        int totalAmount = 0;
+        for (int i = 1; i <= 31; i++) {
+            totalAmount = INITIAL_STAKE;
             while (totalAmount < maxStack && totalAmount > minStack) {
                 double randomNumber = Math.random();
                 System.out.println(i + "Random number is :" + randomNumber);
@@ -28,17 +32,20 @@ public class Gambler {
             }
             System.out.println(i + " day Gambler resign the bet after that he has total amount " + totalAmount);
             if (totalAmount == maxStack) {
-                System.out.println("total amount won at " +i +" is " +(totalAmount-INITIAL_STAKE));
-                winAmount+=(totalAmount-INITIAL_STAKE);
-                winCount+=1;
-            }
-            else {
-                System.out.println("total amount lost at " +i +" is " +(INITIAL_STAKE-totalAmount));
-                lostAmount+=(INITIAL_STAKE-totalAmount);
-                lostCount+=1;
+                System.out.println("total amount won at " + i + " is " + (totalAmount - INITIAL_STAKE));
+                winAmount += (totalAmount - INITIAL_STAKE);
+                winDay=i;
+                winCount += 1;
+            } else {
+                System.out.println("total amount lost at " + i + " is " + (INITIAL_STAKE - totalAmount));
+                lostAmount += (INITIAL_STAKE - totalAmount);
+                lostDay=i;
+                lostCount += 1;
             }
         }
-        System.out.println("you win " +winCount +" days in a month and total win amount is " +winAmount);
-        System.out.println("you lost " +lostCount +" days in a month and total lost amount is " +lostAmount);
+        System.out.println("you win " + winCount + " days in a month and total win amount is " + winAmount);
+        System.out.println("you lost " + lostCount + " days in a month and total lost amount is " + lostAmount);
+        System.out.println("you lukiest day is " +winDay+" you win "+winAmount);
+        System.out.println("your unlukiest day is "+lostDay+" you lost " +lostAmount);
     }
 }
